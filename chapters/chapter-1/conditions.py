57dfe41c31f1cda050a181ca765480b7400519fb
RetrozDev <ryanbarbozapro@gmail.com>
2025-10-08 15:26:07 +0200

    feat(chapter-1): Added condition

---
+++ chapters/chapter-1/conditions.py
@@ -0,0 +1,8 @@
+age = 18
+
+if age >= 18:
+    print("Adult")          # executed if condition is True
+elif age > 12:
+    print("Teenager")       # executed if previous condition False
+else:
+    print("Child")          # executed if all previous conditions False
